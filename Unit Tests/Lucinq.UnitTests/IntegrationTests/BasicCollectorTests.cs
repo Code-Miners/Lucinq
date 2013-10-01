edecbfc4820c37b6ae6e1fa8b98aa09b01f14a13
Simon Proctor <simon@simonproctor.com>
2013-10-01 18:11:48 +0100

    Update date collector to use longs instead of strings

--- Unit Tests/Lucinq.UnitTests/IntegrationTests/BasicCollectorTests.cs
+++ Unit Tests/Lucinq.UnitTests/IntegrationTests/BasicCollectorTests.cs
@@ -39,5 +39,28 @@ namespace Lucinq.UnitTests.IntegrationTests
 
 			Console.WriteLine();
 		}
+
+
+		[Test, TestCaseSource("Searches")]
+		public void CollectDailyWithFilterCount(LuceneSearch luceneSearch)
+		{
+			IQueryBuilder queryBuilder = new QueryBuilder();
+			queryBuilder.Setup
+				(
+					x => x.WildCard(BBCFields.Description, "food"),
+					x => x.Filter(DateRangeFilter.Filter(BBCFields.PublishDateObject, DateTime.Parse("01/02/2013"), DateTime.Parse("28/02/2013")))
+				);
+
+			DateCollector collector = new DateCollector();
+			luceneSearch.Collect(queryBuilder.Build(), collector);
+
+			Assert.Greater(collector.DailyCount.Keys.Count, 0);
+			foreach (String day in collector.DailyCount.Keys)
+			{
+				Console.Error.WriteLine("Day: {0} had {1} documents", day, collector.DailyCount[day]);
+			}
+
+			Console.WriteLine();
+		}
 	}
 }
